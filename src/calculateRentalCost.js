0e5cbccc9d6e913c3fa15600ee3c7a78e5f60cbe
LoverOfHentai <loverofhentai00@gmail.com>
2025-03-18 18:13:43 +0200

    Update calculateRentalCost.js

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -1,10 +1,19 @@
-/**
- * @param {number} days
- *
- * @return {number}
- */
+const SHORT_TERM = 3;
+const SHORT_TERM_DISCOUNT = 20;
+const LONG_TERM = 7;
+const LONG_TERM_DISCOUNT = 50;
+const RENT_COST = 40;
+
 function calculateRentalCost(days) {
-  // write code here
+  if (days >= LONG_TERM) {
+    return days * RENT_COST - LONG_TERM_DISCOUNT;
+  }
+
+  if (days >= SHORT_TERM) {
+    return days * RENT_COST - SHORT_TERM_DISCOUNT;
+  }
+
+  return days * RENT_COST;
 }
 
 module.exports = calculateRentalCost;
